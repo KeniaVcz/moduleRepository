7bdb2d13f51bd454ff85c3de9337a63320d2ea9f
kenia.vargas <kenia.vargas@millicom.com>
2023-10-20 15:26:52 -0600

    add screen

--- lib/global/presentation/screens/example_public_screen.dart
+++ lib/global/presentation/screens/example_public_screen.dart
@@ -4,29 +4,24 @@ class WorkingCapitalExamplePublicScreen extends StatefulWidget {
   const WorkingCapitalExamplePublicScreen({Key? key}) : super(key: key);
 
   @override
-  _WorkingCapitalExamplePublicScreenState createState() => _WorkingCapitalExamplePublicScreenState();
+  _WorkingCapitalExamplePublicScreenState createState() =>
+      _WorkingCapitalExamplePublicScreenState();
 }
 
-class _WorkingCapitalExamplePublicScreenState extends State<WorkingCapitalExamplePublicScreen> {
+class _WorkingCapitalExamplePublicScreenState
+    extends State<WorkingCapitalExamplePublicScreen> {
   // Declarar variables de estado u otras propiedades aquí
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Widget'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Contenido de mi StatefulWidget',
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const <Widget>[
+        Text(
+          'Contenido de mi StatefulWidget',
+          style: TextStyle(fontSize: 20),
         ),
-      ),
+      ],
     );
   }
 }
