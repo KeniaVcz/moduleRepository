02056d14805c38edd20f3a4f27ac7804cc565f51
kenia.vargas <kenia.vargas@millicom.com>
2023-10-20 15:34:32 -0600

    rename

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ library module_repository;
 
 export 'package:module_repository/global/calculator.dart';
 export 'package:module_repository/global/lending_working_capital_module.dart';
-export 'package:module_repository/global/presentation/screens/example_public_screen.dart';
+export 'package:module_repository/global/presentation/screens/my_home_page.dart';
 export 'package:module_repository/global/routers/router_global.dart';
